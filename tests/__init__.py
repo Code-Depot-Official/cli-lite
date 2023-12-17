bde3fd7902c257b99e3634324123b42d78c1ea53
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-12-16 20:02:47 -0800

    test: Key binds and buttons test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,27 @@
 """Runs tests for the CLI Lite App."""
+
+from cli_lite import CLILiteApp
+
+
+async def test_keys() -> None:
+    """Test key binds."""
+    app = CLILiteApp()
+
+    async with app.run_test() as pilot:
+        pilot.press("d")
+        assert not app.dark
+
+        pilot.press("d")
+        assert app.dark
+
+        pilot.press("q")
+        assert not app.is_running
+
+
+async def test_buttons() -> None:
+    """Test buttons."""
+    app = CLILiteApp()
+
+    async with app.run_test() as pilot:
+        pilot.click("#quit")
+        assert not app.is_running
